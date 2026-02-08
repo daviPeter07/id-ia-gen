d4b4192a0d0adf3814a01425d63fb5c997767e41
IdeiaGenAdm <ideiagenstartup@gmail.com>
2026-02-08 15:02:34 -0300

    Add custom headers for asset links and app association

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,18 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async headers() {
+    return [
+      {
+        source: "/.well-known/assetlinks.json",
+        headers: [{ key: "Content-Type", value: "application/json" }],
+      },
+      {
+        source: "/.well-known/apple-app-site-association",
+        headers: [{ key: "Content-Type", value: "application/json" }],
+      },
+    ]
+  },
 }
 
 export default nextConfig
